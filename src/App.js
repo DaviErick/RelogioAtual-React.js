96023558604c45b648e670f2cee2b643c9d87d5b
DaviErick <davierick999@gmail.com>
2023-05-17 15:13:01 -0300

    Novo Projeto
    Novo Projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+// App.js
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [hour, setHour] = useState('');
+  const [minute, setMinutes] = useState('');
+  const [second, setSeconds] = useState('');
+  useEffect(() => {
+    const updateTime = () => {
+      const now = new Date();
+      const formattedHour = now.getHours().toString().padStart(2, '0');
+      const formattedMinutes = now.getMinutes().toString().padStart(2, '0');
+      const formattedSeconds = now.getSeconds().toString().padStart(2, '0');
+      setHour(formattedHour);
+      setMinutes(formattedMinutes);
+      setSeconds(formattedSeconds);
+    };
+
+    updateTime(); // Chama a função para exibir o horário inicial
+
+    const intervalId = setInterval(updateTime, 1000); // Atualiza o horário a cada segundo
+
+    return () => {
+      clearInterval(intervalId); // Limpa o intervalo quando o componente é desmontado
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Relógio virtual usando React</h1>
+      <div className='boxes'>
+        <div className="box-hour"><p>{hour}</p></div>
+        <div className="box-minute"><p>{minute}</p></div>
+        <div className="box-second"><p>{second}</p></div>
+    </div>
     </div>
   );
 }
 
 export default App;
+
